4908cdaac9751b7d9fcf2b5e6bc5420618567556
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-08-31 20:02:41 +0300

    writeContacts.js

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const data = JSON.stringify(updatedContacts, null, 2);
+  try {
+    await fs.appendFile(PATH_DB, data, 'utf8');
+    console.log('Data successfully written to file!');
+  } catch (error) {
+    console.log('Error writing to file: ', error);
+  }
+};
