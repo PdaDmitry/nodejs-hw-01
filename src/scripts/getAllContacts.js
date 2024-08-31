5868a6c2b7fe2ba5a3d6298598541d686647160f
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-08-31 17:17:04 +0300

    readContacts.js

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,17 @@
-export const getAllContacts = async () => {};
+// export const getAllContacts = async () => {};
 
-console.log(await getAllContacts());
+// console.log(await getAllContacts());
+
+import { readContacts } from '../utils/readContacts';
+
+export const getAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    console.log(contacts);
+    return contacts;
+  } catch (error) {
+    console.error('Error fetching contacts:', error);
+  }
+};
+
+getAllContacts();
